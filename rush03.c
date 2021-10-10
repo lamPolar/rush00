03a4f8a296253aa8d233432cb1e2cb4e18229c0c
Heesun Kim <heeskim@c5r1s4.42seoul.kr>
2021-10-10 16:23:42 +0900

    last

--- rush03.c
+++ rush03.c
@@ -6,7 +6,7 @@
 /*   By: heeskim <heeskim@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 10:12:05 by heeskim           #+#    #+#             */
-/*   Updated: 2021/10/10 15:25:30 by heeskim          ###   ########.fr       */
+/*   Updated: 2021/10/10 16:23:27 by heeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 void	ft_putchar(char c);
@@ -24,11 +24,11 @@ void	rush( int x, int y )
 		{
 			if ((row == 1 && col == 1) || (row == y && col == 1))
 				ft_putchar('A');
+			else if ((row == 1 || row == y) && (col != 1 && col != x))
+				ft_putchar('B');
 			else if ((row == 1 && col == x) || (row == y && col == x))
 				ft_putchar('C');
 			else if ((row != 1 && row != y) && (col == 1 || col == x))
-				ft_putchar('B');
-			else if ((col != 1 && col != x) && (row == 1 || row == y))
 				ft_putchar('B');
 			else
 				ft_putchar(' ');
